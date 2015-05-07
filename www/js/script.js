a18a79959a683ca092d22967585eabc5cc3443c4
farahzeb <farahzeb@hotmail.de>
2015-05-07 17:58:26 +0200

    Anzeige der Suraten im Auswahlmenu, noch zu bearbeiten

--- www/js/script.js
+++ www/js/script.js
@@ -1,19 +1,18 @@
-$(function() {
-	// Handler for .ready() called.
-	$.ajax({
-		type: "GET",
-		url: "xml\deutsch.xml",
-		dataType: "xml",
-		success: function(xml) {
-			$(xml).find('sura').each(function(){
-				$("#select-choice-a").append(
-						"<option>" + i+1	+ ":"
-						+ $(this).find('name').text() + 
-				"</option>");
-			});
-		}
-	});
-
-	
-
+$(function() {	
+	 $.ajax({
+         type: "GET",
+         url: 'xml/deutsch.xml',
+         dataType: "xml",
+         success: function(xml) {
+        	 var i = 1;
+         	$(xml).find('sura').each(function(){
+	 			$("#select-choice-a").append("<option value='"+$(this).attr("name")+"'>"+ $(this).find('name').text() +"</option>");
+	 			i++;
+	 		});
+         	$("#select-choice-a").selectmenu("refresh");
+         },error: function(xhr, status, error){
+             console.log(error);
+             console.log(status);
+         }
+     });	 
 });
